eb7621838fa47339c0991bbd3038e9092ba59c9c
Mariia <coldrainLV@gmail.com>
2023-01-31 15:16:59 +0200

    Project setup

---
+++ src/main/java/orange/book/fgpoc/FgpocApplication.java
@@ -0,0 +1,13 @@
+package orange.book.fgpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FgpocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/orange/book/fgpoc/FgpocApplicationTests.java
@@ -0,0 +1,13 @@
+package orange.book.fgpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FgpocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
